e1cf7b9bca4de3261ce3efe5abc21a98b22b1e65
Rob <rob@Rob-Home-Ubuntu.(none)>
2013-04-07 17:56:36 -0400

    Improved README

---
+++ README.md
@@ -0,0 +1,1 @@
+This is the improved README.md file. I had successfully removed the README.rdoc file from this project because ot was pointless.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'd435ddd420280482fd73f9d4edaee694e53d6d5c359361b229029b5381031cfa7e1ad89ecb75cddc02b0f8d1a0f4bf07bde6fe6f0e890f1ad14cd4782a3519c4'
